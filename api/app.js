62ddca1b5106124e30977cdb25589bc148076f35
Eluri Bhulakshmi <eluri.bhulakshmi@capgemini.com>
2023-01-27 17:02:40 +0530

    contactUs Router created

--- api/app.js
+++ api/app.js
@@ -4,6 +4,8 @@ var cookieParser = require('cookie-parser');
 const authRouter = require('./routes/auth');
 const indexRouter = require("./routes");
 const utilityRouter = require('./routes/utils');
+const contactUsRouter = require('./routes/contactUs')
+
 
 const app = express();
 app.use(express.json());
@@ -13,5 +15,6 @@ app.use(cookieParser());
 app.use('/auth', authRouter);
 app.use('/api', indexRouter);
 app.use('/api/utils', utilityRouter);
+app.use('/api/contactUs',contactUsRouter);
 
 module.exports = app;
