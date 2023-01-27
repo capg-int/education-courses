03de8eb2dcfc9db08dda2085ae3828698c70cb03
Eluri Bhulakshmi <eluri.bhulakshmi@capgemini.com>
2023-01-27 17:12:53 +0530

    conflicts resolved

--- api/app.js
+++ api/app.js
@@ -5,13 +5,13 @@ const db = require("./db");
 const authRouter = require('./routes/auth');
 const indexRouter = require("./routes");
 const utilityRouter = require('./routes/utils');
-<<<<<<< HEAD
+
 const contactUsRouter = require('./routes/contactUs')
 
-=======
+
 const aboutRouter = require('./routes/about');
 const courseRouter = require("./routes/course");
->>>>>>> b9fd2c9be3e4def69088a77a148cbcb28b89a962
+
 
 const app = express();
 app.use(express.json());
@@ -32,12 +32,12 @@ app.use(cookieParser());
 app.use('/auth', authRouter);
 app.use('/api', indexRouter);
 app.use('/api/utils', utilityRouter);
-<<<<<<< HEAD
+
 app.use('/api/contactUs',contactUsRouter);
-=======
+
 app.use('/api/about', aboutRouter);
 
 app.use("/api/courses", courseRouter);
->>>>>>> b9fd2c9be3e4def69088a77a148cbcb28b89a962
+
 
 module.exports = app;
